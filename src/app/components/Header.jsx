3a885b43127470af6981677c1bc82133dde14fa2
zim <zim@system32>
2024-09-11 14:42:46 +0800

    Added the fonts, customized the font in tailwind config, finished the header layout [padding, font-sizes, font-family], formatted the navigation string format

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -10,9 +10,9 @@ export default function Header() {
   ];
 
   return (
-    <div className="w-full fixed top-0 right-0 flex flex-col px-[64px] py-[32px] bg-transparent backdrop-blur-sm">
+    <div className="w-full fixed top-0 right-0 flex flex-col px-[64px] py-[32px] backdrop-blur-sm">
       <nav className="max-w-[1400px] mx-auto w-full flex items-center justify-between">
-        <h1>Logo</h1>
+        <h1 className="font-chopsic">tiringbanay</h1>
         <div className="flex text-[14px] font-poppins items-center gap-4">
           {navigations.map((navigation, index) => (
             <Link key={index} href={navigation.href}>
