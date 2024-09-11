5b731808dbaedae1928116f376c777817cd17664
dayummson <educationalpurposesr@gmail.com>
2024-09-11 21:52:16 +0800

    feature/parllax: finished parallax effect and head banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,12 @@ module.exports = {
         foreground: "var(--foreground)",
       },
       fontFamily: {
-        chopsic: ['var(--font-chopsic)']
+        chopsic: ['var(--font-chopsic)'],
       },
-
+      backgroundImage: {
+        'sea2': 'url("https://images.unsplash.com/photo-1524704654690-b56c05c78a00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80")',
+        'tiringbanay': "url('./src/app/public/assets/images/background.png')"
+      }
 
     },
   },
