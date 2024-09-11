762ffd8f2181ecd3a6bf537ddd9c9b05d97b3ee0
zim <zim@system32>
2024-09-11 14:31:03 +0800

    Added the fonts, customized the font in tailwind config, finished the header layout [padding, font-sizes, font-family], formatted the navigation string format

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -10,13 +10,16 @@ export default function Header() {
   ];
 
   return (
-    <div className="w-full fixed top-0 right-0 flex flex-col p-4 bg-transparent backdrop-blur-sm">
+    <div className="w-full fixed top-0 right-0 flex flex-col px-[64px] py-[32px] bg-transparent backdrop-blur-sm">
       <nav className="max-w-[1400px] mx-auto w-full flex items-center justify-between">
         <h1>Logo</h1>
-        <div className="flex items-center gap-4">
+        <div className="flex text-[14px] font-poppins items-center gap-4">
           {navigations.map((navigation, index) => (
             <Link key={index} href={navigation.href}>
-              <span className="text-slate-100">{navigation.name}</span>
+              <span className="text-slate-100">
+                {navigation.name.slice(0, 1).toUpperCase() +
+                  navigation.name.slice(1)}
+              </span>
             </Link>
           ))}
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif']
+      }
     },
   },
   plugins: [],
