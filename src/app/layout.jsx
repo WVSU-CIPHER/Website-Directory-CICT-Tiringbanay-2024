76a3b4f08f010f8becefd153749ee6f35ed60f96
dayummson <educationalpurposesr@gmail.com>
2024-09-11 15:29:20 +0800

    Commit

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,7 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />;{children}
+        <Header />
+        {children}
       </body>
     </html>
   );
