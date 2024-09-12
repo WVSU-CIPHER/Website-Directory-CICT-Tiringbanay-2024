0dbdfe0ba7c9121de40a83ab9ccc26af05501078
dayummson <educationalpurposesr@gmail.com>
2024-09-12 09:42:57 +0800

    Updated the class keyword updated to className

--- src/app/components/Guidelines.jsx
+++ src/app/components/Guidelines.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
+import React from "react";
 
 export default function Guidelines() {
   return (
-    <div class="min-h-screen flex justify-center items-center bg-sea2 bg-fixed bg-no-repeat bg-cover bg-center">
-        <div class="p-10 h-screen bg-transparent bg-opacity-50 backdrop-blur-lg">
-          <h2 class="font-bold text-4xl">Sample Section</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab numquam
-            eveniet molestias veniam, inventore sint voluptates nam provident
-            impedit laborum ipsa vel quos amet laboriosam dolorum suscipit!
-            Nihil eos repellendus blanditiis repellat laboriosam veniam quod
-            maxime ab! Ea eveniet doloremque, excepturi totam, et molestias
-            dicta accusamus quibusdam quas sunt inventore!
-          </p>
-        </div>
+    <div className="min-h-screen flex justify-center items-center bg-sea2 bg-fixed bg-no-repeat bg-cover bg-center">
+      <div className="p-10 h-screen bg-transparent bg-opacity-50 backdrop-blur-lg">
+        <h2 className="font-bold text-4xl">Sample Section</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab numquam
+          eveniet molestias veniam, inventore sint voluptates nam provident
+          impedit laborum ipsa vel quos amet laboriosam dolorum suscipit! Nihil
+          eos repellendus blanditiis repellat laboriosam veniam quod maxime ab!
+          Ea eveniet doloremque, excepturi totam, et molestias dicta accusamus
+          quibusdam quas sunt inventore!
+        </p>
       </div>
-  )
+    </div>
+  );
 }
--- src/app/components/HeroPage.jsx
+++ src/app/components/HeroPage.jsx
@@ -13,9 +13,7 @@ export default function HeroPage() {
       <div className="min-h-screen flex justify-center items-center bg-sea2 bg-fixed bg-no-repeat bg-cover bg-center">
         <div className="flex flex-col text-center">
           <h1
-            className={
-              "${poppins.className} pb-7 tracking-[7px] text-[#EAF7FF] font-medium text-[18px]"
-            }
+            className={`${poppins.className} pb-7 tracking-[7px] text-[#EAF7FF] font-medium text-[18px]`}
           >
             A SHIFT IN REAL<span className="text-[#81ECDE]">IT</span>Y
           </h1>
@@ -28,12 +26,16 @@ export default function HeroPage() {
             </h2>
           </div>
           <div className="flex items-center gap-[16px] justify-center">
-            <button className="{`${poppins.className} leading-[11px] bg-[#2f5057] rounded-[8px] w-[158px] h-[37px] border-[#FFFFFF] border-[1px] tracking-[1px] text-[14px]">
+            <button
+              className={`${poppins.className} leading-[11px] bg-[#2f5057] rounded-[8px] w-[158px] h-[37px] border-[#FFFFFF] border-[1px] tracking-[1px] text-[14px]`}
+            >
               <span className="uppercase text-[#FFFFFF] font-semibold">
                 find your group
               </span>
             </button>
-            <button className="{`${poppins.className} leading-[11px] bg-[#2f5057] rounded-[8px] w-[158px] h-[37px] border-[#FFFFFF] border-[1px] tracking-[1px] text-[14px]">
+            <button
+              className={`${poppins.className} leading-[11px] bg-[#2f5057] rounded-[8px] w-[158px] h-[37px] border-[#FFFFFF] border-[1px] tracking-[1px] text-[14px]`}
+            >
               <span className="uppercase text-[#FFFFFF] font-semibold">
                 About the Event
               </span>
@@ -42,9 +44,9 @@ export default function HeroPage() {
         </div>
       </div>
 
-      <div class="min-h-screen flex justify-center items-center bg-sea2 bg-fixed bg-no-repeat bg-cover bg-center">
-        <div class="p-10 backdrop-blur-lg">
-          <h2 class="font-bold text-4xl">Sample Section</h2>
+      <div className="min-h-screen flex justify-center items-center bg-sea2 bg-fixed bg-no-repeat bg-cover bg-center">
+        <div className="p-10 backdrop-blur-lg">
+          <h2 className="font-bold text-4xl">Sample Section</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab numquam
             eveniet molestias veniam, inventore sint voluptates nam provident
