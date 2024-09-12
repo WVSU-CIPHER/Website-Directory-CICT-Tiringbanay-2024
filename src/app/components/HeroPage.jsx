799bf77e81a4c15297f48e45c78c31279e384945
dayummson <educationalpurposesr@gmail.com>
2024-09-12 09:37:27 +0800

    chaned the next font to next instead of @next

--- src/app/components/HeroPage.jsx
+++ src/app/components/HeroPage.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Poppins } from "@next/font/google";
+import { Poppins } from "next/font/google";
 const poppins = Poppins({
   weight: ["400", "900"],
   variable: "--font-poppins",
@@ -12,19 +12,31 @@ export default function HeroPage() {
     <div>
       <div className="min-h-screen flex justify-center items-center bg-sea2 bg-fixed bg-no-repeat bg-cover bg-center">
         <div className="flex flex-col text-center">
-          <h1 className={"${poppins.className} pb-7 tracking-[7px] text-[#EAF7FF] font-medium text-[18px]"}>
-          A SHIFT IN REAL<span className="text-[#81ECDE]">IT</span>Y
+          <h1
+            className={
+              "${poppins.className} pb-7 tracking-[7px] text-[#EAF7FF] font-medium text-[18px]"
+            }
+          >
+            A SHIFT IN REAL<span className="text-[#81ECDE]">IT</span>Y
           </h1>
           <div className="flex flex-col text-center">
-            <h1 className="text-[72px] font-chopsic text-shadow-custom">tiringbanay</h1>
-            <h2 className="text-[24px] font-chopsic text-shadow-custom pb-16">2024</h2>
+            <h1 className="text-[72px] font-chopsic text-shadow-custom">
+              tiringbanay
+            </h1>
+            <h2 className="text-[24px] font-chopsic text-shadow-custom pb-16">
+              2024
+            </h2>
           </div>
           <div className="flex items-center gap-[16px] justify-center">
             <button className="{`${poppins.className} leading-[11px] bg-[#2f5057] rounded-[8px] w-[158px] h-[37px] border-[#FFFFFF] border-[1px] tracking-[1px] text-[14px]">
-              <span className="uppercase text-[#FFFFFF] font-semibold">find your group</span>
+              <span className="uppercase text-[#FFFFFF] font-semibold">
+                find your group
+              </span>
             </button>
             <button className="{`${poppins.className} leading-[11px] bg-[#2f5057] rounded-[8px] w-[158px] h-[37px] border-[#FFFFFF] border-[1px] tracking-[1px] text-[14px]">
-              <span className="uppercase text-[#FFFFFF] font-semibold">About the Event</span>
+              <span className="uppercase text-[#FFFFFF] font-semibold">
+                About the Event
+              </span>
             </button>
           </div>
         </div>
