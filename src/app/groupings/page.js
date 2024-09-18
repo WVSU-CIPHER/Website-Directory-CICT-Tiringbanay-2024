18386885918ce403c23e9b460be055a76eddba02
dayummson <educationalpurposesr@gmail.com>
2024-09-18 22:07:40 +0800

    updated page.js in groupings

--- src/app/groupings/page.js
+++ src/app/groupings/page.js
@@ -30,7 +30,7 @@ export default function GroupingsPage() {
                                 className="
                                 cursor-pointer flex flex-col w-60 h-80 justify-center items-center
                                 shadow-lg border border-gray-200 m-5 rounded-xl
-                                md:hover:scale-110 hover:bg-gray-100 hover:border-blue-500 hover:shadow-xl duration-100
+                                md:hover:scale-110 hover:bg-gray-100 hover:border-blue-500 hover:shadow-xl duration-100 
                                 ">
                                 <Link href={`/groupings/${group.name}`}>
                                     <Image
