4c10f4ad6f618095fc868b140eaf5dfa53be4248
dayummson <educationalpurposesr@gmail.com>
2024-09-21 00:31:24 +0800

    refactor: finished the programme page [desktop, mobile --layout], fixed the header backdrop filter blur to none

--- src/app/programme/page.js
+++ src/app/programme/page.js
@@ -1,14 +1,103 @@
+"use client";
 
-import Header from "../components/Header"
-import { chopsic } from "../layout"
+import { chopsic } from "../layout";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import bg from "/public/assets/images/background.png"
+
+
+// Fonts
+const poppins = Poppins({
+    weight: ["400", "900"],
+    variable: "--font-poppins",
+    subsets: ["latin"],
+});
+
+// Import data for this page
+import { morningProgramFlow, afternoonProgramFlow, morningTitle, afternoonTitle } from "@/data/programs";
+
+
+
 export default function ProgrammePage() {
     return (
         <div>
-            <Header font={chopsic.variable} />
-            <main className="h-screen flex flex-col w-screen">
-                Programme
-            </main>
-        </div>
-    )
-}
+            <div className="h-screen w-screen overflow-hidden">
+                {/* Background */}
+                <Image
+                    src={bg}
+                    alt="Background"
+                    layout="fill"
+                    objectFit="cover"
+                    quality={100}
+                />
 
+                <div className="relative w-full h-full flex flex-col overflow-y-scroll">
+
+                    <div className=" lg:pb-2 items-center justify-center flex flex-col w-full  md:w-[90%] mx-auto pt-[90px]">
+                        <div className="py-[64px]">
+
+                            <h1 className={`${chopsic.className} text-[40px] text-center`}>
+                                program
+                            </h1>
+                        </div>
+
+                        {/* Cards for program flow */}
+                        <div className="flex flex-col gap-0 md:gap-[64px] w-full z-[200]">
+
+                            {/* Morning session */}
+                            <div className="w-full  md:rounded-2xl px-[64px] h-fit py-[32px] gap-[32px] backdrop-blur-md flex flex-col bg-black/50 ">
+                                <h1 className={`${chopsic.className} text-[24px]`}>{morningTitle}</h1>
+                                {/* timeline */}
+                                <div className="flex flex-col w-full gap-2">
+
+                                    {morningProgramFlow.map((program, index) => (
+
+                                        <button key={index} className="flex justify-between items-center w-full  duration-100 focus:bg-white text-white  focus:text-black rounded-full">
+                                            <div className="max-w-[90%] gap-[32px] grid grid-cols-2 md:flex md:justify-between  text-left w-full">
+
+                                                <p className="px-[12px]">{program.title}</p>
+
+                                                <div className="md:w-1/2 text-left ">
+                                                    <p className="">{program.time}</p>
+
+                                                </div>
+                                            </div>
+                                        </button>
+                                    ))}
+
+                                </div>
+                            </div>
+
+                            {/* Afternoon session */}
+                            <div className="w-full  md:rounded-2xl px-[64px] h-fit py-[32px] gap-[32px] backdrop-blur-md flex flex-col bg-black/50 ">
+                                <h1 className={`${chopsic.className} text-[24px]`}>{afternoonTitle}</h1>
+                                {/* timeline */}
+                                <div className="flex flex-col w-full gap-2">
+
+                                    {afternoonProgramFlow.map((program, index) => (
+
+                                        <button key={index} className="flex justify-between items-center w-full duration-100 focus:bg-white text-white  focus:text-black rounded-full">
+                                            <div className="max-w-[90%] gap-[32px] grid grid-cols-2 md:flex md:justify-between  text-left w-full">
+
+                                                <p className="px-[12px]">{program.title}</p>
+
+                                                <div className="md:w-1/2 text-left ">
+                                                    <p className="">{program.time}</p>
+
+                                                </div>
+
+
+                                            </div>
+                                        </button>
+                                    ))}
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
---
+++ src/data/programs.js
@@ -0,0 +1,23 @@
+export const morningTitle = "morning session"
+export const afternoonTitle = "afternoon session"
+
+export const morningProgramFlow = [
+    { title: 'Attendance', time: '7:00 - 8:00 AM' },
+    { title: 'Preliminaries', time: '8:00 - 8:15 AM' },
+    { title: 'Welcome Remarks', time: '8:15 - 8:30 AM' },
+    { title: 'Sugilanon', time: '8:30 - 10:00 AM' },
+    { title: 'Open Mic', time: '10:00 - 10:30 AM' },
+    { title: 'SPIT (Spontenous Imrpov Theatre', time: '10:30 - 11:30 AM' },
+    { title: 'Video Spoof', time: '11:30 - 12:00 PM' },
+]
+
+
+export const afternoonProgramFlow = [
+    { title: 'Attendance', time: '1:00 - 1:30 AM' },
+    { title: 'Opening Proram', time: '1:30 - 1:45 AM' },
+    { title: 'Welcome Remarks', time: '1:45 - 2:00 AM' },
+    { title: 'Roll Call & Community Dance', time: '2:00 - 3:00 AM' },
+    { title: 'Panakayon', time: '3:00 - 5:00 AM' },
+    { title: 'Awarding', time: '5:00 - 5:30 AM' },
+    { title: 'Sadyahanay', time: '5:30 - 6:30 PM' },
+]
