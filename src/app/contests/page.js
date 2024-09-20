1db9854101701946bcc25af4c2a78dcc29c183aa
dayummson <educationalpurposesr@gmail.com>
2024-09-20 23:36:19 +0800

    fixed missing key when mapping contests/items

--- src/app/contests/page.js
+++ src/app/contests/page.js
@@ -165,7 +165,7 @@ function ContestsOptionPicker() {
         <div className="relative w-fit mx-auto px-[8px] py-[8px] bg-[#5D6D5C] rounded-md max-w-full overflow-hidden">
             <div className="flex gap-[16px] py-[4px] w-full overflow-x-auto scrollbar scrollbar-thumb-white scrollbar-track-[#5D6D5C]">
                 {contestList.map((contest, index) => (
-                    <div className="">
+                    <div className="" key={index}>
                         <input type="radio" id={contest} name="contest" value={contest} key={index} className="hidden peer" />
                         <label for={contest} className="uppercase font-semibold inline-flex items-center w-full p-2 h-full rounded-md border border-transparent peer-checked:bg-[#5A8070] duration-200 cursor-pointer peer-checked:border-[#7AD7C9] text-nowrap"><div className="block">{contest}</div></label>
                     </div>
