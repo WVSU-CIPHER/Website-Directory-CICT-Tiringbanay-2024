4218aa9d297176fb7f8c7793116b3f3a892a0d0d
Christopher-Glenn <christopherglenn.bedis@wvsu.edu.ph>
2024-09-23 19:48:48 +0800

    feat:Contest Page Panagayon Guidelines

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+      browser: true,
+      es6: true,
+    },
+    extends: ['next/core-web-vitals'],
+    rules: {
+        "quotes": ["error", "single"], 
+      },
+  };
+  
