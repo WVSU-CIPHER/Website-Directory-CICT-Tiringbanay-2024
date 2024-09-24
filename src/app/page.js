2dc38cf72d5db77da21389cadf625fe68b16d710
Christopher-Glenn <christopherglenn.bedis@wvsu.edu.ph>
2024-09-24 13:53:27 +0800

    feature:Vercel Analytics

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 import HeroPage from "./components/HeroPage";
-
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
     <main>
-
       <HeroPage />
+      <Analytics />
     </main>
+    
   )
 }
